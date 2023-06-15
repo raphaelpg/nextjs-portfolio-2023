123c536184a0f20c98b1001ca7e2ecb91d7e996a
raphaelpg <raphael_pinto@outlook.com>
2023-06-15 16:33:07 +0200

    update contact section with logos

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="w-full flex center justify-start items-baseline">
-      <h1 className="text-4xl font-bold">16</h1>
+      <h2 className="text-4xl font-bold">16</h2>
       <h3 className="font-bold">-BIT</h3>
     </footer>
   )
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,31 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import GithubLogo from '../images/github.webp'
+import LinkedInLogo from '../images/linkedin.webp'
+
 export default function Contact() {
   return (
-    <div>
-      <div>Phone: 555-555-5555</div>
+    <div className="w-full flex items-center justify-center gap-10">
+      <Link 
+        target='_blank'
+        href={"https://github.com/raphaelpg/"}
+      >
+        <Image 
+          className='bg-gray-100 rounded-lg w-auto h-12'
+          src={GithubLogo} 
+          alt="Github logo" 
+        />
+      </Link>
+      <Link 
+        target='_blank'
+        href={"https://www.linkedin.com/in/raphael-pinto-gregorio-660b2579/"}
+      >
+        <Image 
+          className='bg-gray-100 rounded-lg w-auto h-12'
+          src={LinkedInLogo} 
+          alt="LinkedIn logo" 
+        />
+      </Link>
     </div>
   )
 }
