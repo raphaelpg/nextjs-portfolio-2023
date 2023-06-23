6abc4c3a499e14355c5051f1ffa566ef82d65f54
raphaelpg <raphael_pinto@outlook.com>
2023-06-23 13:56:50 +0200

    link optional in Task

--- app/components/ItemImage.tsx
+++ app/components/ItemImage.tsx
@@ -4,9 +4,10 @@ import Image from "next/image";
 export default function ItemImage({imageSrc, title, projectHref}: {
   imageSrc: string,
   title: string,
-  projectHref: string,
+  projectHref?: string,
 }) {
   return (
+    projectHref ?
     <Link href={projectHref} target='_blank'>
       <Image 
         src={imageSrc} 
@@ -24,5 +25,21 @@ export default function ItemImage({imageSrc, title, projectHref}: {
         }}
       />
     </Link>
+    :
+    <Image 
+      src={imageSrc} 
+      alt={title + " logo"}
+      style={{
+        objectFit: "contain", 
+        width: "auto", 
+        minHeight: "50px",
+        maxWidth: "250px", 
+        minWidth: "250px", 
+        backgroundColor: "white",
+        borderRadius: "0.15rem",
+        padding: "2px",
+        margin: "0 auto",
+      }}
+    />
   )
 }
--- app/components/Task.tsx
+++ app/components/Task.tsx
@@ -3,7 +3,7 @@ import ItemImage from "./ItemImage";
 export default function Task({imageSrc, title, text}: {imageSrc: string, title: string, text?: string[]}) {
   return (
     <span className="flex flex-col md:flex-row lg:flex-row justify-start items-center gap-10">
-      <ItemImage imageSrc={imageSrc} title={title} projectHref=""/>
+      <ItemImage imageSrc={imageSrc} title={title} />
       <span className="w-full flex flex-col justify-center gap-2 text-left">
         <h3 className="text-2xl font-bold tracking-wider">{title}</h3>
         {text && text.map((item:string) => 
